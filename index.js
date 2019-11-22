7ac8c29a5546eb7e79829b769369a685bf4d1df8
codey-singh <bhajanpreet25@gmail.com>
2019-11-21 19:45:52 -0500

    more simple changes

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require("express");
+
+const server = require("http").createServer();
+
+const io = require("socket.io")(server, {
+  path: "/test",
+  serveClient: false,
+  pingInterval: 10000,
+  pingTimeout: 5000,
+  cookie: false
+});
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+server.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
